cd60ed328447a55c521315e7bf6ff1ae42e9616e
Techdoodle <26680599+TheTechdoodle@users.noreply.github.com>
2020-08-05 17:37:34 -0700

    implement reload command

--- src/main/java/com/darkender/plugins/gravitygun/GravityGunCommand.java
+++ src/main/java/com/darkender/plugins/gravitygun/GravityGunCommand.java
@@ -6,7 +6,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,21 +17,57 @@ public class GravityGunCommand implements CommandExecutor, TabCompleter
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
     {
-        if(!(sender instanceof Player))
+        if(args.length == 0 || args[0].equals("get"))
         {
-            sender.sendMessage(ChatColor.RED + "You must be a player to run this command!");
-            return true;
+            if(!sender.hasPermission("gravitygun.cmd.get"))
+            {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to run this command!");
+                return true;
+            }
+            
+            if(!(sender instanceof Player))
+            {
+                sender.sendMessage(ChatColor.RED + "You must be a player to run this command!");
+                return true;
+            }
+    
+            Player player = (Player) sender;
+            player.getInventory().addItem(GravityGun.getGravityGun());
+            sender.sendMessage(ChatColor.GREEN + "Got a gravity gun!");
         }
-        
-        Player player = (Player) sender;
-        player.getInventory().addItem(GravityGun.getGravityGun());
-        sender.sendMessage(ChatColor.GREEN + "Got a gravity gun!");
+        else if(args[0].equals("reload"))
+        {
+            if(!sender.hasPermission("gravitygun.cmd.reload"))
+            {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to run this command!");
+                return true;
+            }
+            
+            GravityGunConfig.reload();
+            sender.sendMessage(ChatColor.GREEN + "Reloaded!");
+        }
         return true;
     }
     
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args)
     {
-        return Collections.emptyList();
+        if(args.length != 1)
+        {
+            return Collections.emptyList();
+        }
+        List<String> options = new ArrayList<>();
+        if(sender.hasPermission("gravitygun.cmd.get"))
+        {
+            options.add("get");
+        }
+        if(sender.hasPermission("gravitygun.cmd.reload"))
+        {
+            options.add("reload");
+        }
+        List<String> completions = new ArrayList<>();
+        StringUtil.copyPartialMatches(args[0], options, completions);
+        Collections.sort(completions);
+        return completions;
     }
 }
